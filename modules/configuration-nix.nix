9a5540ce8bee77be26324d117f50e9cd68cc6266
Tom McLaughlin <tom@codedown.io>
2025-06-04 06:56:13 -0700

    Revert "Fix postgresql-libpq-configure (#2370)"
    This reverts commit 463e08c4cc5af3b892244f978eda9e3986145272.

--- modules/configuration-nix.nix
+++ modules/configuration-nix.nix
@@ -203,7 +203,4 @@ in addPackageKeys {
     pkgs.apple-sdk_11
     (pkgs.darwinMinVersionHook "11.0")
   ];
-
-  packages.postgresql-libpq-configure.components.library.libs = [ (lib.getDev pkgs.postgresql) ]
-    ++ lib.optional (pkgs.postgresql ? pg_config) [ pkgs.postgresql.pg_config ];
 }
