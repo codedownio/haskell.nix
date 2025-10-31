c14e8f0210910f583aa52f11d43d0a5c115f1eb0
Tom McLaughlin <tom@codedown.io>
2025-10-30 20:51:36 -0700

    Add extraFlavourTransformers option to GHC config (closes #2425)

--- compiler/ghc/default.nix
+++ compiler/ghc/default.nix
@@ -62,6 +62,10 @@ let self =
       else "perf-cross-ncg"
     )
 
+, # Extra flavour transformers to pass to Hadrian. For example, +debug_ghc or
+  # +assertions to work on debugging the compiler.
+  extraFlavourTransformers ? []
+
 , # Whether to disable the large address space allocator
   # necessary fix for iOS: https://www.reddit.com/r/haskell/comments/4ttdz1/building_an_osxi386_to_iosarm64_cross_compiler/d5qvd67/
   disableLargeAddressSpace ? stdenv.targetPlatform.isDarwin && stdenv.targetPlatform.isAarch64 || stdenv.targetPlatform.isAndroid
@@ -327,6 +331,7 @@ let
           + lib.optionalString enableIPE "+ipe"
           + lib.optionalString ((enableNativeBignum && hadrianHasNativeBignumFlavour) || targetPlatform.isGhcjs) "+native_bignum"
           + lib.optionalString targetPlatform.isGhcjs "+no_profiled_libs"
+          + lib.concatStrings extraFlavourTransformers
       } --docs=no-sphinx -j --verbose"
       # This is needed to prevent $GCC from emitting out of line atomics.
       # Those would then result in __aarch64_ldadd1_sync and others being referenced, which
